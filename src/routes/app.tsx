876f6a0cc240754c3b09f0734d240e49cf4c1f65
akinloluwami <akinkunmioye42@gmail.com>
2025-12-03 06:19:47 +0100

    fix(app): adjust padding for content area in RouteComponent

--- src/routes/app.tsx
+++ src/routes/app.tsx
@@ -9,7 +9,7 @@ function RouteComponent() {
   return (
     <div className="p-2 bg-gray-100 h-screen gap-2 flex">
       <Sidebar />
-      <div className="bg-white rounded-2xl border border-gray-200 w-full py-2 px-20 overflow-y-auto">
+      <div className="bg-white rounded-2xl border border-gray-200 w-full py-5 px-20 overflow-y-auto">
         <Outlet />
       </div>
     </div>
