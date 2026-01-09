ad0f23815ecc3760924dad8c5b473f40ad96d5d7
akinloluwami <akinkunmioye42@gmail.com>
2026-01-09 22:07:33 +0100

    feat(auth): add organization client to authentication plugins

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,9 +1,18 @@
 import { polarClient } from "@polar-sh/better-auth";
 import { createAuthClient } from "better-auth/react";
-import { emailOTPClient, adminClient } from "better-auth/client/plugins";
+import {
+  emailOTPClient,
+  adminClient,
+  organizationClient,
+} from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-  plugins: [polarClient(), emailOTPClient(), adminClient()],
+  plugins: [
+    polarClient(),
+    emailOTPClient(),
+    adminClient(),
+    organizationClient(),
+  ],
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;
